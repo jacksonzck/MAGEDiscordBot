639dc0d2b6408ff370e8838e04cc561c1cfaae65
jacksonzck <detterbeckjackson@gmail.com>
2022-03-03 21:05:32 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# With a lot of help from https://github.com/parafoxia/hikari-intro/blob/main/tanjun_bot/__main__.py
+
+import hikari
+import os
+import tanjun
+
+GUILD_ID = 123123123 # CHANGE LATER
+
+def create_bot() -> hikari.GatewayBot:
+    with open("./secrets/token") as f:
+        pass
+
+# We're doing it man
+# We're making it happen
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
